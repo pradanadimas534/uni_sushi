abd423f9e26730c3da58366933086ea0625cb03e
pradanadimas534 <pradanadimas534@gmail.com>
2026-07-21 15:54:31 +0700

    perbaikan 7

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import 'swiper/css/navigation';
 const rupiah = (n) => 'Rp ' + Number(n || 0).toLocaleString('id-ID');
 
 // Nyalakan mode maintenance dengan set ke true.
-const MAINTENANCE_MODE = (window.MAINTENANCE_MODE);
+const MAINTENANCE_MODE = false;
 
 function Maintenance({ content, wa }) {
   return (
